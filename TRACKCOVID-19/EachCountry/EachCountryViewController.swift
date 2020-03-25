f15f3e7f590dc3b27da941a0f42f7748dce51b23
ebukaebunam <ebukaebunam@gmail.com>
2020-03-25 17:28:18 -0500

    added a popview , added datalist from api , added segmented control , worked omn segmented comntrol functions , still working on popview

--- TRACKCOVID-19/EachCountry/EachCountryViewController.swift
+++ TRACKCOVID-19/EachCountry/EachCountryViewController.swift
@@ -45,6 +45,10 @@ class EachCountryViewController: UIViewController {
     
     @IBOutlet weak var segmentedView: UIView!
     
+    @IBOutlet weak var bStack: UIStackView!
+    
+    var popView = UIView()
+    var originalPopUpY : CGFloat?
     
     let font = UIFont(descriptor: UIFontDescriptor(fontAttributes: [UIFontDescriptor.AttributeName.name: "ArialRoundedMTBold"]), size: 16)
     
@@ -52,7 +56,28 @@ class EachCountryViewController: UIViewController {
     var lastUpdateTableView = UITableView()
     var ProvinceTableView = UITableView()
     
+    var firstList = ["hey","how","hey"]
+    var secondList = ["bye","boy","babe", "bbaba"]
+    
+    var dataList : [stat_by_country] = []
+    
+   lazy var  tableViewList = dataList
+    
     override func viewDidLoad() {
+        
+        DataService.sharedClient.testEndPointCountry(Country: countryInformation?.country ?? "") { [weak self](data) in
+            if let data = data{
+                let stats = data.stat_by_country
+                self?.dataList = stats
+                DispatchQueue.main.async {
+                    self?.tableViewList = self!.dataList
+                    self?.lastUpdateTableView.reloadData()
+                }
+            }else{
+                print("data is nil")
+            }
+            
+        }
         super.viewDidLoad()
         title = countryInformation?.country
         setupView()
@@ -86,7 +111,10 @@ extension EachCountryViewController : CountryListVCDelegate{
 extension EachCountryViewController {
     
     func setupView(){
-        segmentedView.isHidden = true
+         //segmentedView.isHidden = true
+        //segmentedView.addSubview(setupsegmantedControl())
+        bStack.addSubview(setupsegmantedControl())
+        segmentedView.backgroundColor = .white
         //UIColor.init(hex: Constants.lightblue)?.withAlphaComponent(0.8)
         setupdeathView()
         setuprecoveryView()
@@ -96,13 +124,8 @@ extension EachCountryViewController {
         setupTopView()
         setupBottomView()
         setupLastUpdateTableView()
-        //setupsegmantedControl()
+        setuppopView()
         
-        
-        
-        DataService.sharedClient.testEndPointCountry(Country: countryInformation?.country ?? "") { (_) in
-            
-        }
     }
     
     func setupLastUpdateTableView(){
@@ -115,6 +138,7 @@ extension EachCountryViewController {
         
     }
     
+   
     func setupdeathView(){
         guard let countryInformation = countryInformation else {return}
         
@@ -124,7 +148,45 @@ extension EachCountryViewController {
         deathsImage.contentMode = .scaleAspectFit
         deathsLabel.text = "Death: \(deathToll)"
         deathsLabel.font = font
-         deathsLabel.adjustsFontSizeToFitWidth = true
+        deathsLabel.adjustsFontSizeToFitWidth = true
+        
+    }
+    
+    func setuppopView(){
+        let width = view.frame.width - 150
+        let height = view.frame.height - 550
+        let x = (view.frame.width - width)/2
+        let y = view.frame.height
+        originalPopUpY = y
+        popView.frame = CGRect(x: x, y: y, width: width, height: height)
+        popView.layer.cornerRadius = 10
+        popView.backgroundColor = .black
+        view.addSubview(popView)
+
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+        button.backgroundColor = .white
+        button.addTarget(self, action: #selector(handlepopcancelButton), for: .touchUpInside)
+        button.setImage(UIImage(named: "cancel.png"), for: .normal)
+        popView.addSubview(button)
+        
+    }
+    
+    @objc func handlepopcancelButton(){
+        removepopUp()
+    }
+    
+    func displaypopView(){
+        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseIn, animations: {
+            self.popView.frame.origin.y = self.segmentedView.frame.midY
+        }, completion: nil)
+    }
+    
+    func removepopUp(){
+        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.9, initialSpringVelocity: 0, options: .curveEaseIn, animations: {
+            if let y = self.originalPopUpY{
+                 self.popView.frame.origin.y = y
+            }
+        }, completion: nil)
         
     }
     
@@ -174,22 +236,41 @@ extension EachCountryViewController {
     }
     
     func setupsegmantedControl() -> BetterSegmentedControl{
+        let width = segmentedView.frame.width - 10
+        let x = (view.frame.width - width)/2
         let control = BetterSegmentedControl(
             frame: segmentedView.frame,
-        segments: LabelSegment.segments(withTitles: ["One", "Two"],
-        normalFont:font,
-        normalTextColor: .lightGray,
-        selectedFont:font,
-        selectedTextColor: .white),
-        index: 0,
-        options: [.backgroundColor(.white),
-                  .indicatorViewBackgroundColor(UIColor.init(hex: Constants.lightblue) ?? UIColor.lightGray),
-        .cornerRadius(12.0)
-            ,
-        .animationSpringDamping(0.7),
-        .panningDisabled(true)
+            segments: LabelSegment.segments(withTitles: ["One", "Two"],
+                                            normalFont:font,
+                                            normalTextColor: .lightGray,
+                                            selectedFont:font,
+                                            selectedTextColor: .white),
+            index: 0,
+            options: [.backgroundColor(.white),
+                      .indicatorViewBackgroundColor(UIColor.init(hex: Constants.lightblue) ?? UIColor.lightGray),
+                      .cornerRadius(15.0)
+                ,
+                      .animationSpringDamping(0.7),
+                      .panningDisabled(true)
         ])
+       
+        control.addTarget(self, action: #selector(segmentedControlFuntion(_:)), for: .valueChanged)
         return control
+        
+    }
+    
+    
+    @objc func segmentedControlFuntion(_ sender : BetterSegmentedControl){
+        if sender.index == 0{
+           tableViewList = dataList
+            
+        }
+        else{
+            print("index is 1")
+           tableViewList = dataList
+            
+        }
+        lastUpdateTableView.reloadData()
         
     }
     
@@ -204,6 +285,8 @@ extension EachCountryViewController {
         bottomView.backgroundColor = .white
         
     }
+    
+    
     func setcountryImage(name : String)-> String{
         var newname = name
         if newname == "USA"{
@@ -222,18 +305,26 @@ extension EachCountryViewController {
 
 extension EachCountryViewController : UITableViewDelegate , UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        
+        return tableViewList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-       
-        return UITableViewCell()
+        let cell = UITableViewCell()
+        cell.textLabel?.text = tableViewList[indexPath.row].record_date
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        displaypopView()
     }
     
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        return setupsegmantedControl()
+        return UILabel()
+        
     }
-    
-    
+
+
 }
